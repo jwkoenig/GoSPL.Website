4b796c8dcb58041513020069c061a41d1958e826
jwkoenig <163156903+jwkoenig@users.noreply.github.com>
2026-09-21 15:20:08 +0200

    Fix typo in WorkGrid.js description

--- components/WorkGrid.js
+++ components/WorkGrid.js
@@ -32,7 +32,7 @@ export default function WorkGrid({ onOpen }) {
           <p>
             We create interactive scenes to explore interactively online.
             <br />
-            Straight in your browser, no installation, no logn waiting times.
+            Straight in your browser without install.
           </p>
         </div>
 
